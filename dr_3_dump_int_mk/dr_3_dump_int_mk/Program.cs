009c7e956a71e4a42fcf8a779d3bc26518ba7a11
marija2143 <karoglan.marija9@gmail.com>
2024-11-20 23:07:14 +0100

    basics change, clearer logic, start dictionary added

--- dr_3_dump_int_mk/dr_3_dump_int_mk/Program.cs
+++ dr_3_dump_int_mk/dr_3_dump_int_mk/Program.cs
@@ -37,24 +37,36 @@ namespace dr_3_dump_int_mk
             }
             return first_choice;
         }
-
-        static void Main(string[] args)
+        static Dictionary<Project, List<Task>> GetStarted() 
         {
-            //declaring string arrays for (sub)menus
-            string[] main_menu = {"Projekti i zadaci","Dodaj novi projekt","Izbrisi projekt","Zadaci s rokom u sljedecih 7 dana",
-                "Projekti prema statusu","Uredi specificni projekt","Uredi specificni zadatak" };
-            string[] sub_menu_6 = {"Prikazi zadatke","Prikazi detalje","Uredi status","Dodaj zadatak","Izbrisi zadatak","Prikazi predvideno vrijeme do kraja" };
-            string[] sub_menu_7 = { "Prikazi detalje","Uredi status"};
+            var start_dict = new Dictionary<Project, List<Task>>();
+            var list_p1 = new List<Task>();
+            var list_p2 = new List<Task>();
 
-            
-            //dictionary , key project, value tasklist
-            //MAIN MENU
-            Console.WriteLine("Izbornik: ");
-            for (int i = 0; i < main_menu.Length; i++)
-            {
-                Console.WriteLine($"{i+1} - {main_menu[i]}");
-            }
-            var main_choice=Check_Menu(main_menu.Length);
+            var proj1 = new Project("Titula 1", "Opis 1", DateTime.Parse("01/11/2024"), DateTime.Parse("15/07/2025"), "active");
+            var proj2 = new Project("Titula 2", "Opis 2", DateTime.Parse("02/11/2024"), DateTime.Parse("25/012/2025"), "active");
+
+            var task1 = new Task("Titula 1 1", "Opis 1 1", DateTime.Parse("11/5/2025"), 150, "aktivan", proj1);
+            var task2 = new Task("Titula 1 2", "Opis 1 2", DateTime.Parse("25/11/2024"), 130, "aktivan", proj1);
+            var task3 = new Task("Titula 1 3", "Opis 1 3", DateTime.Parse("12/12/2024"), 120, "aktivan", proj1);
+            list_p1.Add(task1);
+            list_p1.Add(task2);
+            list_p1.Add(task3);
+
+            var task4 = new Task("Titula 2 1", "Opis 2 1", DateTime.Parse("15/12/2024"), 111, "aktivan", proj2);
+            var task5 = new Task("Titula 2 2", "Opis 2 2", DateTime.Parse("25/5/2025"), 250, "aktivan", proj2);
+            list_p2.Add(task4);
+            list_p2.Add(task5);
+
+            start_dict.Add(proj1, list_p1);
+            start_dict.Add(proj2, list_p2);
+
+            return start_dict;
+        }
+        static void Functions_Choice(int main_choice,string[] sub_menu_6, string[] sub_menu_7)
+        {
+            //starting dictionary , key project, value tasklist
+            var dict = GetStarted();
 
             switch (main_choice)
             {
@@ -83,7 +95,7 @@ namespace dr_3_dump_int_mk
                     Console.WriteLine("Podizbornik: ");
                     for (int i = 0; i < sub_menu_6.Length; i++)
                     {
-                        Console.WriteLine($"{i+1}) {sub_menu_6[i]}");
+                        Console.WriteLine($"{i + 1}) {sub_menu_6[i]}");
                     }
                     var sub_6_choice = Check_Menu(sub_menu_6.Length);
                     Console.WriteLine("opcija 6");
@@ -91,17 +103,41 @@ namespace dr_3_dump_int_mk
                 case 7:
                     //option 7 - editing specific task
                     Console.WriteLine("Podizbornik: ");
-                    for (int i = 0; i < main_menu.Length; i++)
+                    for (int i = 0; i < sub_menu_7.Length; i++)
                     {
-                        Console.WriteLine($"{i+1}) {sub_menu_7[i]}");
+                        Console.WriteLine($"{i + 1}) {sub_menu_7[i]}");
                     }
                     var sub_7_choice = Check_Menu(sub_menu_7.Length);
                     Console.WriteLine("opcija 7");
                     break;
+                case 8:
+                    Console.Clear();
+                    Console.WriteLine("Dovidenja! Pritisnite bilo koju tipku za izlaz.");
+                    Environment.Exit(0);
+                    break;
                 default:
                     break;
             }
+        }
+        static void Start() 
+        {
+            //declaring string arrays for (sub)menus
+            string[] main_menu = {"Projekti i zadaci","Dodaj novi projekt","Izbrisi projekt","Zadaci s rokom u sljedecih 7 dana",
+                "Projekti prema statusu","Uredi specificni projekt","Uredi specificni zadatak" };
+            string[] sub_menu_6 = { "Prikazi zadatke", "Prikazi detalje", "Uredi status", "Dodaj zadatak", "Izbrisi zadatak", "Prikazi predvideno vrijeme do kraja" };
+            string[] sub_menu_7 = { "Prikazi detalje", "Uredi status" };
 
+
+            //MAIN MENU
+            Console.WriteLine("Izbornik: ");
+            for (int i = 0; i < main_menu.Length; i++)
+            {
+                Console.WriteLine($"{i + 1} - {main_menu[i]}");
+            }
+            Console.WriteLine((main_menu.Length+1)+" - Izlaz");
+            var main_choice = Check_Menu(main_menu.Length+1);
+
+            Functions_Choice(main_choice, sub_menu_6, sub_menu_7);
             //            Pri implementaciji je važno vodit računa da dva projekta ne smiju imat isti naziv,
             //            te unutar projekta dva zadatka ne smiju imat isti naziv.
             //
@@ -118,6 +154,11 @@ namespace dr_3_dump_int_mk
             //
             //          Potrebno je da prilikom pokretanja aplikacije postoje inicijalni podaci s kojima je moguće obavljati sve navedene funkcionalnosti koje aplikacija nudi.
             //          Isto tako aplikacije ne smije “pucati” prilikom korištenja.
+        }
+
+        static void Main(string[] args)
+        {
+            Start();
         }
     }
 }
--- dr_3_dump_int_mk/dr_3_dump_int_mk/Task.cs
+++ dr_3_dump_int_mk/dr_3_dump_int_mk/Task.cs
@@ -17,17 +17,17 @@ namespace dr_3_dump_int_mk
         //status active, on hold or done
         public string status;
         //expected work time in minutes
-        //private TimeSpan.Minutes {get;} workload;
+        public int workload;
 
         //related project
         public Project project;
-        public Task(string title_, string description_, DateTime deadline_, string status_,Project project_)
+        public Task(string title_, string description_, DateTime deadline_,int workload_ ,string status_,Project project_)
         {
             this.title = title_;
             this.description = description_;
             this.deadline = deadline_;
             this.status = status_;
-            //this.workload=workload;
+            this.workload=workload_;
             this.project = project_;
 
         }
